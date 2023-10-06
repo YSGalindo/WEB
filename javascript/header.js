f9799f5d71d87211acfdbee1d8733c085da34476
adrivilgo <elnegritosamuel@hotmail.com>
2023-10-05 20:34:40 -0500

    header actualizado

---
+++ javascript/header.js
@@ -0,0 +1,15 @@
+let  data = [
+    "zapatos",
+    "blusas",
+    "pantalones",
+    "sudaderas",
+    "vestidos",
+    "tenis"
+
+ ];
+ function buscar() {
+
+    let query = document.getElementById("buscar").value;
+      console.log(query);
+      document.getElementById("buscar").value = " "
+ } 
